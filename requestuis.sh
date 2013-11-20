3b97c356172649fb4cab58863038c49c1dc13b3d
Eric Winkelman <e.winkelman@cablelabs.com>
2013-11-20 14:41:01 -0700

    fix test script, added SOAPACTION

--- requestuis.sh
+++ requestuis.sh
@@ -1,5 +1,5 @@
-curl --header "content-type: application/soap+xml" --data '<?xml version="1.0" encoding="utf-8"?>
-<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding">
+curl --header "content-type: application/soap+xml" -H "SOAPAction: urn:schemas-upnp-org:service:RemoteUIServer:1#GetCompatibleUIs" --data '<?xml version="1.0" encoding="utf-8"?>
+<s:Envelope s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" xmlns:s="http://schemas.xmlsoap.org/soap/envelope/">
  <s:Body>
   <u:GetCompatibleUIs xmlns:u="urn:schemas-upnp-org:service:RemoteUIServer:1">
    <InputDeviceProfile/>
